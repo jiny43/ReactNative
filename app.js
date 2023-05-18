877c2041ff4cd4c5770845028118e49d171b73dc
jungjieun <je_eun2735@naver.com>
2023-05-18 15:21:22 +0900

    익스프레스 세계로 출발~

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const http = require('http')
-const port = process.env.PORT
+const { type } = require('os')
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200, {'Content-type':'text/plain'})
+  res.end('hello')
+})
+server.listen(port,() => console.log('서버 작동됨'))
